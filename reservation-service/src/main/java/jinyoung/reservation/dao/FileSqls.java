55be9f96a80f63d5e777b11985aaed20cd309fd9
sejun <riding1516@gmail.com>
2017-07-26 11:16:42 +0900

    commit test

--- reservation-service/src/main/java/jinyoung/reservation/dao/FileSqls.java
+++ reservation-service/src/main/java/jinyoung/reservation/dao/FileSqls.java
@@ -5,4 +5,5 @@ public class FileSqls {
 			+ "FROM product_image, file where product_image.file_id = file.id and product_id = :productId;";
 	public static final String SELECT_IMAGE_BY_FILE_ID = "SELECT file.id file_id, file_name, save_file_name, file_length, content_type\n"
 			+ "FROM file where file.id = :fileId;";
+	//test
 }
