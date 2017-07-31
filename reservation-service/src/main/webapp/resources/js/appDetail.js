b0ecc89a01a19d76246b8be244b6fe16287a336a
sejun <riding1516@gmail.com>
2017-07-31 16:45:55 +0900

    맞추기

--- reservation-service/src/main/webapp/resources/js/appDetail.js
+++ reservation-service/src/main/webapp/resources/js/appDetail.js
@@ -1,93 +1,69 @@
 (function(window) {
   'use strict';
   //header 영역
-
   var headModule = (function() {
-
     $(".container_visual").find("img").css('width', '414');
     $(".container_visual").find("img").css('height', '414');
   })();
-
   $('.bk_btn').on('click', function() {
     window.location.href = window.location.href + '/reserve'
   })
-
-
-
   // 모듈 인스턴스화 필요
   var PopUpModule = (function() {
     var $findElement = $('.img-popup-layer.img-viewer');
     var $ulEle = $findElement.find('ul');
     var $thumb = $('.thumb');
-
     var $countElement;
-
     var itemSource = $('#popup-img-template').html();
     var itemTemplate = Handlebars.compile(itemSource);
-
     var isDragging = false;
-    var isDowning = false;
-    var curImage = 0; 
-
     var curX;
-    var originOffset = 0;
+    var originOffset;
     var images;
+    var curImage;
     var count;
     var commentId;
     var dX;
     var move;
     var autoMoveSize;
-<<<<<<< Updated upstream
-
     function init($th) {
       curImage = 0;
       count = 0;
-=======
-    //$findElement.find('img').attr('src', $(this).find('img').attr('src'));
-    function init($th){
->>>>>>> Stashed changes
       $countElement = $('.img-popup-layer.count span');
-      images;
+      images = new Array();
       commentId = $th.attr('id');
       count = $th.siblings('.img_count').text();
       $countElement.text('1 / ' + count);
     }
-
     $thumb.on('click', function(e) {
       init($(this));
       $('div.img-popup-layer').show();
       popupImgList();
     });
-
     $('.img-popup-layer.exit').on('click', function() {
       $('div.img-popup-layer').hide();
       removeLi();
     });
-
     function popupImgList() {
       $.ajax({
         type: 'GET',
         url: '/api/comments/pictures/' + commentId,
         contentType: 'application/json',
         success: function(res) {
-          images = new Array();
           for (var i = 0; i < res.length; i++) {
             images[i] = res[i];
           }
           makeList(res);
-          //bindtouchEvent();
+          bindtouchEvent();
         }
       });
       $ulEle.css({
         "left": 0 + "%"
       });
     }
-
     function removeLi() {
       $ulEle.children().remove();
-      curImage = 0;
     }
-
     function makeList(res) {
       var liEle = [];
       for (var i = 0; i < res.length; i++) {
@@ -101,62 +77,29 @@
       move = $ulEle.width();
       autoMoveSize = $ulEle.width() / 3;
     }
-<<<<<<< Updated upstream
-
-
     function bindtouchEvent() {
       $ulEle.on('mousedown touchstart', function(e) {
         isDragging = true;
-
-=======
-    
-     
-    //function bindtouchEvent(){
-
-      $findElement.on('mousedown touchstart', function(e) {
-        this.isDragging = true;
-        this.isDowning = true;
->>>>>>> Stashed changes
         if (e.type == 'touchstart') {
           e = e.touches[0];
         }
-<<<<<<< Updated upstream
         curX = e.clientX;
         console.log('curX = ' + curX);
       });
       $ulEle.on('mousemove touchmove', function(e) {
         if (isDragging) {
-=======
-        //console.log($(this).find('ul'));
-        this.originOffset = $(this).find('ul').css('left').split('p')[0];
-        //console.log("down : "+this.originOffset);
-        this.curX = e.clientX - this.originOffset;
-        //console.log(e.clientX +"-"+ originOffset + "=" + curX);
-      }).bind(PopUpModule);
-
-      $findElement.on('mousemove touchmove', function(e) {
-        if (this.isDragging) {
->>>>>>> Stashed changes
           if (e.type == 'touchmove') {
             e = e.touches[0];
           }
-          this.dX = e.clientX - this.curX;
-          //console.log(this.dX +  "=" + e.clientX + "-" + this.curX );
+          dX = e.clientX - curX;
           $ulEle.css({
-<<<<<<< Updated upstream
             "left": -curImage * 100 + (dX / 30) + "%"
-=======
-            "left": this.dX+"px"
->>>>>>> Stashed changes
           });
-
         }
-<<<<<<< Updated upstream
       });
       $ulEle.on('dragstart', function() {
         return false;
       });
-
       $(document).on('mouseup touchend', function(e) {
         if (isDragging) {
           isDragging = false;
@@ -175,47 +118,12 @@
         }
       });
     }
-
     function imageMove(e) {
       var inDecre = -dX / Math.abs(dX);
-=======
-      }).bind(PopUpModule);
-
-      $ulEle.on('dragstart',function(){
-        return false;
-      });
-
-      $findElement.on('mouseup touchend', function(e) {
-        this.isDragging = false;
-        
-        this.dX = e.clientX - this.originOffset - this.curX;
-        console.log(this.dX +"="+e.clientX+"-"+this.originOffset+"-"+this.curX);
-        isDowning = false;
-        if (this.dX < -200 || 200 < this.dX ) {
-        
-          if(!imageMove(e))
-            return false;
-        } else{
-          console.log("?? : " + this.originOffset);
-          $ulEle.css({
-            "left": this.originOffset+"px"
-          });
-        }
-      }).bind(PopUpModule);
-    //}
-
-    function imageMove(e){
-      var inDecre = -dX/Math.abs(dX);
-      console.log("curImage : "+curImage + " inDecre= -" + dX + "/"+Math.abs(dX));
->>>>>>> Stashed changes
       curImage += inDecre;
       if (curImage < count && curImage >= 0) {
         $countElement.text((curImage + 1) + ' / ' + count);
         isDragging = false;
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
         $ulEle.animate({
           "left": -curImage * 100 + "%"
         }, 'normal');
@@ -229,15 +137,10 @@
       if (e.type == 'mousemove') {
         e.preventDefault();
       }
-      originOffset = originOffset-inDecre*(move);
-      return true;
     }
   })();
-
-
   //이미지 슬라이드 부분
   var ImageRollerModule = (function() {
-
     var $findElement = $('.container_visual').find('ul');
     var $imageContainer = $('.container_visual');
     var $prev = $('.btn_prev');
@@ -250,9 +153,7 @@
     var isChanged = false;
     var curX;
     var images = new Array();
-
     $('.figure_pagination .num.off span').html(pages);
-
     $findElement.on('mousedown touchstart', function(e) {
       isDragging = true;
       if (e.type == 'touchstart') {
@@ -266,7 +167,6 @@
         imageMove(dX);
       }
     });
-
     $findElement.on('mouseup touchend', function(e) {
       isDragging = false;
       isChanged = false;
@@ -282,7 +182,6 @@
         }
       }
     });
-
     $next.on({
       'click': function() {
         if (clickState === false) {
@@ -293,7 +192,6 @@
         }
       }
     });
-
     function imageMove(dX) {
       if (dX > -(currentPage - 1) * move + 30) { // >
         isDragging = false;
@@ -304,10 +202,8 @@
           return;
         }
       } else if (dX < -(currentPage - 1) * move - 30) { // <
-
         isDragging = false;
         isChanged = true;
-
         if (currentPage < pages) {
           currentPage++;
           translate(currentPage);
@@ -315,11 +211,9 @@
         }
       }
     }
-
     function translate(page) {
       clickState = true;
       $('.figure_pagination .num:first-child').html(page);
-
       $findElement.animate({
         'left': -(page - 1) * move + 'px'
       }, 'normal', function() {
@@ -327,8 +221,6 @@
       });
     }
   })();
-
-
   var buttonModule = (function() {
     // 공유하기
     $('.btn_goto_share').on('click', function() {
@@ -336,7 +228,6 @@
       var title = encodeURI($('.visual_txt').find('span').html());
       document.location = "http://share.naver.com/web/shareView.nhn?url=" + sharingUrl + "&title=" + title;
     });
-
     //펼쳐보기 클릭 시
     $('.bk_more').on('click', function() {
       if ($('.store_details').hasClass('close3')) {
@@ -350,8 +241,6 @@
       }
       $(window).scrollTop($('.section_store_details').offset().top);
     })
-
-
     //상세정보, 오시는길
     $('.section_info_tab .anchor').on('click', function() {
       if (!$(this).hasClass('active')) {
@@ -367,14 +256,11 @@
       }
       $(window).scrollTop($('.section_info_tab').offset().top);
     });
-
     //Footer Top
     $('.lnk_top').click(function() {
       $(this).scrollTop(0);
     });
-
   })();
-
   //지도부분
   var Location = (function() {
     var x;
@@ -401,12 +287,7 @@
       $('.store_location').on('click', function(e) {
         window.location.href = 'http://map.naver.com/index.nhn?query=' + urlencode(myaddress);
       });
-
-
     });
   })();
-
-
   $("img.img_thumb").lazyload();
-
 })(window);
